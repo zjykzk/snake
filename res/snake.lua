77915ba59c9e3bacf7f3cff95699b054314e3592
zhangkai <zhangkai.zju@gmail.com>
2012-08-03 18:21:40 +0800

    add some logic

--- res/snake.lua
+++ res/snake.lua
@@ -1,29 +1,35 @@
 snake = {
-  width = 100,
-  height = 100,
+  width = 300,
+  height = 300,
 
   Up = 0,
   Down = 1,
   Left = 2,
   Right = 3,
+  Step = 10,
 
   x = 100, y = 100,
   map={}
 }
+
 snake.move = function (direction)
   x, y = snake.x, snake.y
   if snake.Left == direction then
-    x = x - 10
+    x = x - snake.Step
   elseif snake.Right == direction then
-    x = x + 10
+    x = x + snake.Step
   elseif snake.Up == direction then
-    y = y - 10
+    y = y - snake.Step
   elseif snake.Down == direction then
-    y = y + 10
+    y = y + snake.Step
   else
-    x = x + 10
+    x = x + snake.Step
   end
   delRect()
+  if x > snake.width then x = 100 end
+  if x < 0 then x = snake.width end
+  if y > snake.height then y = 100 end
+  if y < 0 then y = snake.height end
   drawRect(x, y)
   snake.x, snake.y = x, y
 end
@@ -35,5 +41,11 @@ for w = 1, snake.width do
   end
 end
 
-drawRect(snake.x,snake.y)
-drawRect(snake.x+10,snake.y)
+local function init()
+	for i = 0, 6 do
+		drawRect(snake.x + i * snake.Step, snake.y)
+	end
+	snake.x = snake.x + 6 * snake.Step
+end
+
+init()
